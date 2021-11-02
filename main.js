151c8636a15d824d40235768f6c0de99174eba41
Jessica Nelson <yahwehschica@yahoo.com>
2021-11-02 17:01:31 -0400

    commit tree 1ec95ff6433c98f651456eb118ec34cc479aac74

--- main.js
+++ main.js
@@ -1,8 +1,38 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const modal = document.querySelector("#modal")
+const hearts = document.getElementsByClassName("like-glyph")
+const modalMessage = document.querySelector("#modal-message")
 
-// Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  likePost(hearts)
+ })
+
+const likePost = (hearts) => {
+  for(const heart of hearts) {
+    heart.addEventListener("click", () => {
+      mimicServerCall()
+      .then(()=> {
+        if (heart.innerHTML === EMPTY_HEART) {
+          heart.innerHTML= FULL_HEART
+          heart.className = "activated-heart"
+        } else {
+          heart.innerHTML ===  FULL_HEART
+          heart.className = "like-glyph"
+          }
+        })
+        .catch(error => {
+          modalMessage.hidden = false 
+          modalMessage.innerText = error
+          setTimeout( () => {
+            modal.hidden = true
+          }, 3000)
+        })
+      })
+    } 
+  }
+
 
 
 
